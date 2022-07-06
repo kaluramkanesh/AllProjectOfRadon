77988feabd45bd520230ff960f790782a512bf5b
Susnik20 <anikh725@gmail.com>
2022-07-06 13:01:51 +0530

    user model added

---
+++ src/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+
+        title: {type :String, required: true, enum: [Mr, Mrs, Miss]},
+        name: {type :String, required : true},
+        phone: {type : String, required : true, unique : true},
+        email: {type :String, required : true, unique : true}, 
+        password: {type :String, required : true },
+        address: {
+          street: String,
+          city: String,
+          pincode: String
+        }
+      
+},{timestamps : true})
+
+mongoose.exports = mongoose.model('user', userSchema)
